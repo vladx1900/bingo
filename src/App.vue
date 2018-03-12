d4c1ff7c3470523c780fc3b9983c0ba11116653d
Catalin Vlad <Catalin.Vlad@payu.ro>
2018-03-12 18:10:33 +0200

    game functional

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,28 @@
 
       <div class="game-board">
 
-        <table>
-          <tr v-for="row in 6">
-            <td class="cell" v-for="column in 7"><div id="circle"></div></td>
+        <table id="table">
+          <tr v-for="row in 7">
+            <template v-if="row === 1">
+              <td class="arrow invisible" v-for="column in 7"><img src="./assets/arrow.png" style="height: 40px; width: 70px;"></td>
+            </template>
+            <template v-if="row !== 1">
+              <td class="cell" v-for="column in 7">
+                <div class="circle" :style="{'background-color': circleColorInit}" @mouseover="showArrow(column)" @mouseleave="hideArrow(column)" @click="markCell($event, row, column)"></div>
+              </td>
+            </template>
+
           </tr>
         </table>
 
 
       </div>
       <div class="table-end-board"></div>
+
+      <div>
+        <template v-if="player1"><h4>RED player's turn:</h4></template>
+        <template v-if="player2"><h4>YELLOW player's turn:</h4></template>
+      </div>
 
     </div>
 
@@ -34,7 +47,18 @@
       name: 'App',
       data: function () {
           return {
-              'isGameActive': false
+              'isGameActive': false,
+              'matrix': [
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0]
+                  ],
+              player1: true,
+              player2: false,
+              circleColorInit: 'aliceblue'
           }
       },
       methods: {
@@ -43,7 +67,148 @@
           },
           giveUp: function () {
               this.isGameActive = false;
+          },
+          showArrow: function (column) {
+              var cell = this.selectCell(1, column);
+              cell.setAttribute('class', 'arrow');
+          },
+          hideArrow: function (column) {
+              var cell = this.selectCell(1, column);
+              cell.setAttribute('class', 'arrow invisible');
+          },
+          markCell: function (event, row, column) {
+
+              if ( ((row-2) === 5 || this.matrix[row-1][column-1] !== 0) && this.isGameActive) {
+
+                  if (this.matrix[row - 2][column - 1] === 0) {
+
+                      if (this.player1) {
+                          this.matrix[row - 2][column - 1] = 1;
+                          event.target.style.backgroundColor = 'red';
+                          this.checkForWinner(row-2, column-1, 1);
+                          this.player1 = false;
+                          this.player2 = true;
+                      } else {
+                          this.matrix[row - 2][column - 1] = 2;
+                          event.target.style.backgroundColor = 'yellow';
+                          this.checkForWinner(row-2, column-1, 2);
+                          this.player1 = true;
+                          this.player2 = false;
+                      }
+                  }
+
+              } else {
+                  console.log("error");
+              }
+
+          },
+          selectCell: function (row, column) {
+              var table = $("#table")[0];
+              var cell = table.rows[row-1].cells[column-1];
+              return cell;
+          },
+          checkForWinner: function (row, column, player) {
+              var x;
+              var y;
+              var winCondition;
+
+              //case 1
+              x = row;
+              y = column;
+              winCondition = 1;
+
+              while((y-1)>=0 && this.matrix[x][y-1] === player) {
+                  y--;
+                  winCondition++;
+              }
+              y = column;
+              while((y+1)<7 && this.matrix[x][y+1] === player) {
+                  y++;
+                  winCondition++;
+              }
+
+              if(winCondition >=4) {
+                  this.showSwal(player);
+                  return true;
+              }
+
+              //case 2
+              x= row;
+              y = column;
+              winCondition = 1;
+
+              while((x-1)>=0 && this.matrix[x-1][y] === player) {
+                  x--;
+                  winCondition++;
+              }
+              x = row;
+              while((x+1)<6 && this.matrix[x+1][y] === player) {
+                  x++;
+                  winCondition++;
+              }
+              if(winCondition >=4) {
+                  this.showSwal(player);
+                  return true;
+              }
+
+              //case 3
+              x = row;
+              y = column;
+              winCondition = 1;
+
+              while((x-1)>=0 && (y+1)<7 && this.matrix[x-1][y+1] === player) {
+                  x--;
+                  y++;
+                  winCondition++;
+              }
+              x = row;
+              y = column;
+              while((x+1)<6 && (y-1)>=0 && this.matrix[x+1][y-1] === player) {
+                  x++;
+                  y--;
+                  winCondition++;
+              }
+              if(winCondition >= 4) {
+                  this.showSwal(player);
+                  return true;
+              }
+
+              //case 4
+              x = row;
+              y = column;
+              winCondition = 1;
+
+              while((x-1)>=0 && (y-1)>=0 && this.matrix[x-1][y-1] === player) {
+                  x--;
+                  y--;
+                  winCondition++;
+              }
+              x = row;
+              y = column;
+              while((x+1)<6 && (y+1)<7 && this.matrix[x+1][y+1] === player) {
+                  x++;
+                  y++;
+                  winCondition++;
+              }
+              if(winCondition >= 4) {
+                  this.showSwal(player);
+                  return true;
+              }
+
+              return false;
+
+
+
+
+          },
+          showSwal: function (player) {
+              if(player === 1) {
+                  swal('RED player wins!');
+              } else {
+                  swal('YELLOW player wins!');
+              }
           }
+
       }
   }
 </script>
@@ -58,7 +223,12 @@
     width: 100px;
     height: 100px;
     background-color: blue;
-    border: 1px solid;
+    border: 2px solid;
+  }
+
+  .arrow {
+    width: 130px;
+    height: 70px;
   }
 
   .game-board {
@@ -66,8 +236,8 @@
     padding-left: 20%;
     padding-top: 5%;
   }
-  #circle:hover {
-    background-color: chartreuse;
+  .circle:hover {
+    background-color: chartreuse !important;
   }
 
   .table-end-board {
@@ -76,10 +246,16 @@
     background-color: sienna;
     box-shadow: 0px 5px 5px grey;
   }
-  #circle {
+  .circle {
     width: 100px;
     height: 100px;
-    background: aliceblue;
+    border-radius: 50%;
+  }
+
+  .circle-red {
+    width: 100px;
+    height: 100px;
+    background-color: sienna;
     -moz-border-radius: 50px;
     -webkit-border-radius: 50px;
     border-radius: 50px;
